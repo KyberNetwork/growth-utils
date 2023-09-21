a1d0de991168ad16c83b85b3df1495f9442714e0
Zohar97 <zohar@kyber.network>
2023-09-21 15:29:56 +0700

    fix address input param

--- contracts/KSRescue.sol
+++ contracts/KSRescue.sol
@@ -10,7 +10,7 @@ abstract contract KSRescue is KyberSwapRole {
 
   address private constant ETH_ADDRESS = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
 
-  function rescueFunds(IERC20 token, uint256 amount, address recipient) external onlyOwner {
+  function rescueFunds(address token, uint256 amount, address recipient) external onlyOwner {
     require(recipient != address(0), 'KSRescue: invalid recipient');
     if (amount == 0) amount = _getAvailableAmount(token);
     if (amount > 0) {
@@ -18,21 +18,21 @@ abstract contract KSRescue is KyberSwapRole {
         (bool success,) = recipient.call{value: amount}('');
         require(success, 'KSRescue: ETH_TRANSFER_FAILED');
       } else {
-        token.safeTransfer(recipient, amount);
+        IERC20(token).safeTransfer(recipient, amount);
       }
     }
   }
 
-  function _getAvailableAmount(IERC20 token) internal view virtual returns (uint256 amount) {
+  function _getAvailableAmount(address token) internal view virtual returns (uint256 amount) {
     if (_isETH(token)) {
       amount = address(this).balance;
     } else {
-      amount = token.balanceOf(address(this));
+      amount = IERC20(token).balanceOf(address(this));
     }
     if (amount > 0) --amount;
   }
 
-  function _isETH(IERC20 token) internal pure returns (bool) {
-    return (address(token) == ETH_ADDRESS);
+  function _isETH(address token) internal pure returns (bool) {
+    return (token == ETH_ADDRESS);
   }
 }
